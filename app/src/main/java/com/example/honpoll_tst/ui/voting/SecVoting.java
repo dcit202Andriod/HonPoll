8f1ffac3f3f43fd43a22e52d4a70b45165d049dd
KAsare1 <asarek977@gmail.com>
2023-07-10 22:56:22 +0000

    proper definition of voting types

--- app/src/main/java/com/example/honpoll_tst/ui/voting/SecVoting.java
+++ app/src/main/java/com/example/honpoll_tst/ui/voting/SecVoting.java
@@ -1,13 +1,26 @@
 package com.example.honpoll_tst.ui.voting;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import com.example.honpoll_tst.MainActivity;
 import com.example.honpoll_tst.R;
 
 public class SecVoting extends Activity {
+
+    private Button savencont;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secvoting);
-}}
+
+        initUI();
+
+        savencont.setOnClickListener(view -> startActivity(new Intent(SecVoting.this, MainActivity.class)));
+}
+    public void initUI(){
+        savencont = findViewById(R.id.savencont);
+    }
+}
